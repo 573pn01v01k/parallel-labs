ff7e921775df852872dc419e0bedc54911e71a38
Pavel Litovchenko <pavel>
2020-10-31 01:47:59 +0300

    Scripted auto-commit on change (2020-10-31 01:47:59) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
@@ -10,5 +10,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
     private static final int DEST_AIRPORT_ID = 14;
     private static final int ARIVAL_DELAY = 18;
 
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        
+    }
 }
