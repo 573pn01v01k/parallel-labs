9cbb1c8940a57055991374e7c36031961790cfb5
Pavel Litovchenko <pavel>
2020-10-31 02:41:17 +0300

    Scripted auto-commit on change (2020-10-31 02:41:17) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportReducer.java
@@ -3,7 +3,12 @@ package ru.bmstu.parralel.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
 
-public class AirportReducer extends Reducer<AirportKey, Text, Text, Text> throws {
 
+public class AirportReducer extends Reducer<AirportKey, Text, Text, Text> {
+    @Override
+    protected void reduce(AirportKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
