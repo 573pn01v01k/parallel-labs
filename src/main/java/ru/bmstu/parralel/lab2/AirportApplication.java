6f08b1305760deb509151ef91be121150b01c292
Pavel Litovchenko <pavel>
2020-10-31 00:56:47 +0300

    Scripted auto-commit on change (2020-10-31 00:56:47) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
@@ -1,8 +1,8 @@
 package ru.bmstu.parralel.lab2;
 
 public class AirportApplication {
-    public static void main(String[] args) throws Exeption{
-        if(arg.length != 3){
+    public static void main(String[] args) throws Exception{
+        if(args.length != 3){
             System.err.println("Use correct arguments");
             System.exit(-1);
         }
