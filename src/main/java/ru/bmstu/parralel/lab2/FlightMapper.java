70d32f375370d062080084d30193703d68f02959
Pavel Litovchenko <pavel>
2020-10-31 01:46:21 +0300

    Scripted auto-commit on change (2020-10-31 01:46:21) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
     private static final int DEST_AIRPORT_ID = 14;
     private static final int ARIVAL_DELAY = 18;
+    
 }
 
 
