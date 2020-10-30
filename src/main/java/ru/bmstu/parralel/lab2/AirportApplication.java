40c850ae20ed10c4cf347a9e5e868978c109836e
Pavel Litovchenko <pavel>
2020-10-31 02:17:31 +0300

    Scripted auto-commit on change (2020-10-31 02:17:31) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
@@ -1,6 +1,7 @@
 package ru.bmstu.parralel.lab2;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -17,7 +18,10 @@ public class AirportApplication {
 
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
-        
 
+        job.setMapOutputKeyClass(AirportKey.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setPartitionerClass(AirportPartitioner.class);
     }
 }
