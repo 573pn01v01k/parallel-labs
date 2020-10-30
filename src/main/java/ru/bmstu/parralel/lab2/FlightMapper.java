60629bb980e6e9c1438620ef6ff6c64a8e22ba32
Pavel Litovchenko <pavel>
2020-10-31 02:06:50 +0300

    Scripted auto-commit on change (2020-10-31 02:06:50) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
@@ -3,6 +3,7 @@ package ru.bmstu.parralel.lab2;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
 
 import java.io.IOException;
 
@@ -10,18 +11,22 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
     private static final int DEST_AIRPORT_ID = 14;
     private static final int ARRIVAL_DELAY = 18;
 
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String fields[];
         int airportID;
         float arrivalDelay;
 
-        if(key.get() == 0){
+        if (key.get() == 0) {
             return;
         }
+        arrivalDelay = 0;
         fields = value.toString().replace("\"", "").split(",");
-        arrivalDelay = Float.parseFloat(fields[ARRIVAL_DELAY]);
-        if(!arrivalDelay == "", {
-            arrivalDelay =
+        if (!fields[ARRIVAL_DELAY].isEmpty()) {
+            arrivalDelay = Float.parseFloat(fields[ARRIVAL_DELAY]);
+        }
+        if(arrivalDelay > 0){
+            airportID = Integer.parseInt(fields[DEST_AIRPORT_ID]);
+            context.write(new AirportKey(airportID, false))
         }
     }
 }
