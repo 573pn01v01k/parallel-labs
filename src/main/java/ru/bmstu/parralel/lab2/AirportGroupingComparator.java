0a0416c14f8b9070a8ae8bd78f66779c3e2c873e
Pavel Litovchenko <pavel>
2020-10-31 02:33:59 +0300

    Scripted auto-commit on change (2020-10-31 02:33:59) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportGroupingComparator.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportGroupingComparator.java
@@ -1,8 +1,16 @@
 package ru.bmstu.parralel.lab2;
 
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
+    AirportGroupingComparator(){
+        super(AirportKey.class, true);
+    }
 
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        return super.compare(a, b);
+    }
 }
