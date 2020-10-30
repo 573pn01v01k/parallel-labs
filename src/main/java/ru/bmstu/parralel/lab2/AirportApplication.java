a5820dd8a08fabb3961b72f44a7d50601ade4aef
Pavel Litovchenko <pavel>
2020-10-31 00:59:20 +0300

    Scripted auto-commit on change (2020-10-31 00:59:20) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
@@ -1,10 +1,17 @@
 package ru.bmstu.parralel.lab2;
 
+import org.apache.hadoop.mapreduce.*;
+
 public class AirportApplication {
     public static void main(String[] args) throws Exception{
         if(args.length != 3){
             System.err.println("Use correct arguments");
             System.exit(-1);
         }
+        Job job = Job.getInstance();
+        job.setJarByClass(AirportApplication.class);
+        job.setJobName("AirportApplication");
+        
+
     }
 }
