21143102c0f86a9a539a66d22abae3bdf7029a6a
Pavel Litovchenko <pavel>
2020-10-31 01:11:59 +0300

    Scripted auto-commit on change (2020-10-31 01:11:59) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportKey.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportKey.java
@@ -1,5 +1,26 @@
 package ru.bmstu.parralel.lab2;
 
-public class AirportKey {
-    
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportKey implements WritableComparable<AirportKey>{
+    @Override
+    public int compareTo(AirportKey o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
+} {
+
 }
