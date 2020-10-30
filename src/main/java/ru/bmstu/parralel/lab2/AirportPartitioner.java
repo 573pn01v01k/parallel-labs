af46ea97eea1cdcbd3eb624615d884512d4a7a42
Pavel Litovchenko <pavel>
2020-10-31 02:18:41 +0300

    Scripted auto-commit on change (2020-10-31 02:18:41) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportPartitioner.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportPartitioner.java
@@ -1,5 +1,12 @@
 package ru.bmstu.parralel.lab2;
 
-public class AirportPartitioner extends Partitioner<AirportKey, {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
 
+public class AirportPartitioner extends Partitioner<AirportKey, Text> {
+
+    @Override
+    public int getPartition(AirportKey airportKey, Text text, int i) {
+        return 0;
+    }
 }
