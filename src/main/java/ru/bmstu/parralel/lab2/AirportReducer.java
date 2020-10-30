3e93b2d4b6c1c4763f5edb2e645344a120ca3830
Pavel Litovchenko <pavel>
2020-10-31 02:40:18 +0300

    Scripted auto-commit on change (2020-10-31 02:40:18) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportReducer.java
@@ -2,5 +2,5 @@ package ru.bmstu.parralel.lab2;
 
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirportReducer extends Reducer {
+public class AirportReducer extends Reducer<AirportKey, Text, > {
 }
