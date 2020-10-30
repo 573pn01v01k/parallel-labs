cb9cf5198ccd72a5307f2f17e8473f31536e2cd0
Pavel Litovchenko <pavel>
2020-10-31 01:47:24 +0300

    Scripted auto-commit on change (2020-10-31 01:47:24) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
@@ -4,12 +4,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
     private static final int DEST_AIRPORT_ID = 14;
     private static final int ARIVAL_DELAY = 18;
 
-    protected void
+    protected void map(LongWritable key, Text value, Context context) throws IOException
 }
-
-
-
