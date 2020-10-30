277f2deaba25deb9e3462e938a34e586b2e78d58
Pavel Litovchenko <pavel>
2020-10-31 01:42:04 +0300

    Scripted auto-commit on change (2020-10-31 01:42:04) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/parralel/lab2/FlightMapper.java
@@ -1,8 +1,12 @@
 package ru.bmstu.parralel.lab2;
 
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightMapper extends Mapper<LongWritable, Text, 
-        > {
-
+public class FlightMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
+    
 }
+
+
+
