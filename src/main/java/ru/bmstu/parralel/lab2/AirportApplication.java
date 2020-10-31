7cfa885af401126d137d713e85623f11030ad5d8
Pavel Litovchenko <pavel>
2020-10-31 03:21:16 +0300

    Scripted auto-commit on change (2020-10-31 03:21:16) by gitwatch.sh

--- src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
+++ src/main/java/ru/bmstu/parralel/lab2/AirportApplication.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirportApplication {
     public static void main(String[] args) throws Exception{
@@ -18,6 +19,7 @@ public class AirportApplication {
 
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         job.setMapOutputKeyClass(AirportKey.class);
         job.setMapOutputValueClass(Text.class);
